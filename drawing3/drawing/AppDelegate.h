c56d9b0d3f62197ce6ef5961eb84cf7a2fef13e5
Chernenkov <zxc_ghoul@1hokage.ru>
2022-05-11 02:01:33 +0300

    task2-1

---
+++ drawing3/drawing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  drawing
+//
+//  Created by Abobus on 5/11/22.
+//  Copyright © 2022 Abobus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ drawing3/drawing/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  drawing
+//
+//  Created by Abobus on 5/11/22.
+//  Copyright © 2022 Abobus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
